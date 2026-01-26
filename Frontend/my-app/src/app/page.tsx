16735e9ee016a8448e827d833b14877f8b6bb6fb
Codeshop277 <codeshop277@gmail.com>
2026-01-26 15:51:22 +0530

    Structure

--- Frontend/my-app/src/app/page.tsx
+++ Frontend/my-app/src/app/page.tsx
@@ -1,12 +1,16 @@
 import Navigation from "../components/Navigation";
 import HeroSection from "../components/HeroSection";
-import DashboardCards from "..//components/DashboardCards";
-import FeaturesSection from "..//components/FeaturesSection";
-import EmployeeInsights from "..//components/EmployeeInsights";
+import DashboardCards from "../components/DashboardCards";
+import FeaturesSection from "../components/FeaturesSection";
+import EmployeeInsights from "../components/EmployeeInsights";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-slate-600 via-slate-700 to-slate-800">
+     <div className="bg-red-500 text-white p-4">
+  TESTssss
+</div>
+
       <Navigation />
       <HeroSection />
       <DashboardCards />
