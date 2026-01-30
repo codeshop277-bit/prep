fe15155c4da7ed9d6be2065d41e7f5a06a215a7b
Codeshop277 <codeshop277@gmail.com>
2026-01-30 19:08:03 +0530

    Oauth

--- Backend/src/middleware/validate.middleware.js
+++ Backend/src/middleware/validate.middleware.js
@@ -34,3 +34,88 @@ const authenticate = (req, res, next) => {
 };
 module.exports = {validate, authenticate};
 
+const authorize = (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Forbidden" });
+    }
+    next();
+  };
+};
+// ?Oauth
+const { Issuer } = require("openid-client");
+
+let client;
+
+async function initClient() {
+  const issuer = await Issuer.discover(
+    "http://localhost:8080/realms/myrealm"
+  );
+
+  client = new issuer.Client({
+    client_id: "app-client",
+    client_secret: "your-client-secret",
+    redirect_uris: ["http://localhost:3000/callback"],
+    response_types: ["code"]
+  });
+}
+
+function getClient() {
+  return client;
+}
+
+module.exports = { initClient, getClient };
+
+const express = require("express");
+const session = require("express-session");
+const { initClient, getClient } = require("./auth");
+
+const app = express();
+
+app.use(session({
+  secret: "super-secret",
+  resave: false,
+  saveUninitialized: false
+}));
+
+(async () => {
+  await initClient();
+})();
+
+app.get("/login", (req, res) => {
+  const client = getClient();
+
+  const authUrl = client.authorizationUrl({
+    scope: "openid profile email"
+  });
+
+  res.redirect(authUrl);
+});
+
+app.get("/callback", async (req, res) => {
+  const client = getClient();
+
+  const params = client.callbackParams(req);
+  const tokenSet = await client.callback(
+    "http://localhost:3000/callback",
+    params
+  );
+
+  const userInfo = await client.userinfo(tokenSet.access_token);
+
+  req.session.user = userInfo;
+
+  res.redirect("/");
+});
+
+app.get("/", (req, res) => {
+  if (!req.session.user) {
+    return res.redirect("/login");
+  }
+
+  res.send(`Hello ${req.session.user.name} from App A`);
+});
+
+app.listen(3000, () => {
+  console.log("App A running on http://localhost:3000");
+});
